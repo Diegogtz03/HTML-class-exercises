243b0accfb5fd425b892969e2168471f3e40f67f
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-05-07 10:59:46 -0600

    fix: refactor navigation actions and header component, update character API functions, and add constants and utilities

--- 04-nestjs-rick-and-morty/src/app/actions.ts
+++ 04-nestjs-rick-and-morty/src/app/actions.ts
@@ -1,6 +1,6 @@
-'use server'
- 
-import { redirect } from 'next/navigation'
+"use server"
+
+import { redirect } from "next/navigation"
 
 export async function navigateToCharacter(id: number) {
   redirect(`/character/${id}`)
